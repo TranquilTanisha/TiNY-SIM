37979c5bace6aec02ee5956d04096771098b8a05
TranquilTanisha <tanisha.kaur@somaiya.edu>
2023-03-24 10:27:30 +0530

    .

--- pel/models.py
+++ pel/models.py
@@ -6,7 +6,7 @@ class Encode(models.Model):
     image = models.ImageField(upload_to='encoded/', default='images/test2.png')
     filename=models.CharField(max_length=100)
     message = models.TextField()
-    key=models.CharField(max_length=10, default=None)
+    key=models.TextField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
     def __str__(self):
@@ -16,7 +16,7 @@ class Decode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='images/')
     message = models.TextField()
-    key=models.CharField(max_length=10, default="")
+    key=models.TextField()
 
     def __str__(self):
         return self.message
--- pel/urls.py
+++ pel/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("encode/", views.encode, name="encode"),
-    path("download/<str:pk>/", views.download, name="download"),
-
     path("decode/", views.decode, name="decode"),
 ]
